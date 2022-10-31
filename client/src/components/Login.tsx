1faeada4261c13f469bd39fa920553a4d774bc1b
emdiso <emdisomma@gmail.com>
2022-10-31 09:48:52 -0400

    organized and added components

---
+++ client/src/components/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function Login() {
+  return (
+    <div className="Login">
+      <p>login works</p>
+    </div>
+  );
+}
+
+export default Login;
---
+++ client/src/components/Signup.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function Signup() {
+  return (
+    <div className="Signup">
+      <p>login works</p>
+    </div>
+  );
+}
+
+export default Signup;
