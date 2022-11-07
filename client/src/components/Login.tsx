677ead3b76c4fd6596f10fd9e972b3d762b52f1d
omrichashper <114836028+omrichashper@users.noreply.github.com>
2022-11-07 09:19:13 -0500

    Refactored Components to Functional React

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 
-class Login extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {};
-  // }
   
-  render() {
-    return (
-      <div className="Login">
-        <p>login works</p>
-      </div>
-    );
-  };
-}
+const Login = () => {
+  return (
+    <div className="Login">
+      <p>login works</p>
+    </div>
+  );
+};
 
 export default Login;
--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 
-class Signup extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {};
-  // }
-
-  render() {
-    return (
-      <div className="Signup">
-        <p>signup works</p>
-      </div>
-    );
-  };
-}
+const Signup = () => {
+  return (
+    <div className="Signup">
+      <p>signup works</p>
+    </div>
+  );
+};
 
 export default Signup;
