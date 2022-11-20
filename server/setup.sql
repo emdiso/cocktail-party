7576908fb1d04dc4b3be52be5e22f31d7db6cf71
cjc436 <97143146+cjc436@users.noreply.github.com>
2022-11-20 18:08:13 -0500

    adjusted tables to match api, removed extra recipe table

--- server/setup.sql
+++ server/setup.sql
@@ -30,54 +30,48 @@ CREATE TABLE menus (
         REFERENCES images(id)
         ON DELETE SET NULL
 );
-CREATE TABLE recipes ( -- recipes from Api (use api_recipe_id to get recipe data from api)
-	id SERIAL PRIMARY KEY,
-	api_recipe_id INT
-	-- ingredients ARRAY, -- list of ingredients
-	-- measurements ARRAY -- list of measurements
-);
-CREATE TABLE custom_recipes (
+CREATE TABLE custom_recipes ( -- I made them look like the external api's data (even though we all dislike their naming/structure)
 	id SERIAL PRIMARY KEY,
 	user_id INT NOT NULL,
 	image_id INT,
-	drink VARCHAR(100),
-	is_alcoholic BOOLEAN,
-	category VARCHAR(50),
-	glass VARCHAR(50),
-	instructions VARCHAR(300),
+	"strDrink" VARCHAR(100),
+	"strAlcoholic" VARCHAR(50),
+	"strCategory" VARCHAR(50),
+	"strGlass" VARCHAR(50),
+	"strInstructions" VARCHAR(300),
 	-- ingredients ARRAY, -- list of ingredients
 	-- measures ARRAY, -- json list of measurements
-	ingredient1 VARCHAR(50),
-	ingredient2 VARCHAR(50),
-	ingredient3 VARCHAR(50),
-	ingredient4 VARCHAR(50),
-	ingredient5 VARCHAR(50),
-	ingredient6 VARCHAR(50),
-	ingredient7 VARCHAR(50),
-	ingredient8 VARCHAR(50),
-	ingredient9 VARCHAR(50),
-	ingredient10 VARCHAR(50),
-	ingredient11 VARCHAR(50),
-	ingredient12 VARCHAR(50),
-	ingredient13 VARCHAR(50),
-	ingredient14 VARCHAR(50),
-	ingredient15 VARCHAR(50),
-	measure1 VARCHAR(50),
-	measure2 VARCHAR(50),
-	measure3 VARCHAR(50),
-	measure4 VARCHAR(50),
-	measure5 VARCHAR(50),
-	measure6 VARCHAR(50),
-	measure7 VARCHAR(50),
-	measure8 VARCHAR(50),
-	measure9 VARCHAR(50),
-	measure10 VARCHAR(50),
-	measure11 VARCHAR(50),
-	measure12 VARCHAR(50),
-	measure13 VARCHAR(50),
-	measure14 VARCHAR(50),
-	measure15 VARCHAR(50),
-	date_modified DATE,
+	"strIngredient1" VARCHAR(50),
+	"strIngredient2" VARCHAR(50),
+	"strIngredient3" VARCHAR(50),
+	"strIngredient4" VARCHAR(50),
+	"strIngredient5" VARCHAR(50),
+	"strIngredient6" VARCHAR(50),
+	"strIngredient7" VARCHAR(50),
+	"strIngredient8" VARCHAR(50),
+	"strIngredient9" VARCHAR(50),
+	"strIngredient10" VARCHAR(50),
+	"strIngredient11" VARCHAR(50),
+	"strIngredient12" VARCHAR(50),
+	"strIngredient13" VARCHAR(50),
+	"strIngredient14" VARCHAR(50),
+	"strIngredient15" VARCHAR(50),
+	"strMeasure1" VARCHAR(50),
+	"strMeasure2" VARCHAR(50),
+	"strMeasure3" VARCHAR(50),
+	"strMeasure4" VARCHAR(50),
+	"strMeasure5" VARCHAR(50),
+	"strMeasure6" VARCHAR(50),
+	"strMeasure7" VARCHAR(50),
+	"strMeasure8" VARCHAR(50),
+	"strMeasure9" VARCHAR(50),
+	"strMeasure10" VARCHAR(50),
+	"strMeasure11" VARCHAR(50),
+	"strMeasure12" VARCHAR(50),
+	"strMeasure13" VARCHAR(50),
+	"strMeasure14" VARCHAR(50),
+	"strMeasure15" VARCHAR(50),
+	"dateModified" VARCHAR(50),
 
 	CONSTRAINT FK_Custom_Recipe_User FOREIGN KEY(user_id)
         REFERENCES users(id)
@@ -89,15 +83,12 @@ CREATE TABLE custom_recipes (
 CREATE TABLE menu_items (
 	id SERIAL PRIMARY KEY,
 	menu_id INT NOT NULL,
-	recipe_id INT, -- null if item is custom recipe
+	api_recipe_id INT, -- null if item is custom recipe
 	custom_recipe_id INT, -- null if item is normal recipe
 
 	CONSTRAINT FK_Item_Menu FOREIGN KEY(menu_id)
         REFERENCES menus(id)
         ON DELETE CASCADE,
-	CONSTRAINT FK_Item_Recipe FOREIGN KEY(recipe_id)
-        REFERENCES recipes(id)
-        ON DELETE SET NULL,
 	CONSTRAINT FK_Item_Custom_Recipe FOREIGN KEY(custom_recipe_id)
         REFERENCES custom_recipes(id)
         ON DELETE SET NULL
--- server/utils/cocktailApiUtils.ts
+++ server/utils/cocktailApiUtils.ts
@@ -11,9 +11,46 @@ export interface Menu {
 }
 
 export interface MenuItem {
-    recipe_id?: number;
+    id: number;
+    api_recipe_id?: number;
     custom_recipe_id?: number;
-    // add attributes of recipe tables
+    // add attributes of recipe tables -- I made them look like the external api's data (even though we all dislike their naming/structure)
+	image_id?: number;
+	strDrink: string;
+	strAlcoholic: string;
+	strCategory: string;
+	strGlass: string;
+    strInstructions: string;
+    strIngredient1: string;
+    strIngredient2: string;
+    strIngredient3: string;
+    strIngredient4: string;
+    strIngredient5: string;
+    strIngredient6: string;
+    strIngredient7: string;
+    strIngredient8: string;
+    strIngredient9: string;
+    strIngredient10: string;
+    strIngredient11: string;
+    strIngredient12: string;
+    strIngredient13: string;
+    strIngredient14: string;
+    strIngredient15: string;
+    strMeasure1: string;
+    strMeasure2: string;
+    strMeasure3: string;
+    strMeasure4: string;
+    strMeasure5: string;
+    strMeasure6: string;
+    strMeasure7: string;
+    strMeasure8: string;
+    strMeasure9: string;
+    strMeasure10: string;
+    strMeasure11: string;
+    strMeasure12: string;
+    strMeasure13: string;
+    strMeasure14: string;
+    strMeasure15: string;
 }
 
 export const retreiveAllMenuInfo = async (menuId: string, userId: string) => {
@@ -29,13 +66,19 @@ export const retreiveAllMenuInfo = async (menuId: string, userId: string) => {
     const menu: Menu = menuResult.rows[0];
 
     const menuItemsResult: QueryResult = await psqlPool.query(
-        `SELECT mi.recipe_id, mi.custom_recipe_id
-         FROM menu_items mi
+        `SELECT mi.id, mi.api_recipe_id, cr.id as custom_recipe_id, cr.*
+         FROM menu_items mi LEFT JOIN custom_recipes cr ON mi.custom_recipe_id = cr.id
          WHERE mi.menu_id = $1`,
         [ menuId ]);
     if (menuResult.rowCount > 0) {
         menu["menu_items"] = menuItemsResult.rows;
+        for (const item of menu["menu_items"]) {
+            if (item.api_recipe_id !== null) {
+                // make api call to external
+            }
+        }
     }
+    
 
     return menu;
 };
