0421dc64204685e84b070eb9f23ca7b3d448e6c9
emdiso <emdisomma@gmail.com>
2022-12-05 22:18:59 -0500

    makin moves

--- dist/utils/psqlConnection.js
+++ dist/utils/psqlConnection.js
@@ -1,9 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const pg_1 = require("pg");
-const psqlEnv = require("../../env.json"); // Convert to typsecript import statment
-let psqlPool = new pg_1.Pool(psqlEnv);
-psqlPool.connect().then(() => {
-    console.log("Connected to database");
+const Pool = require("pg").Pool;
+require("dotenv").config();
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
-exports.default = psqlPool;
+// const psqlEnv = require("../../env.json"); // Convert to typsecript import statment
+// let psqlPool = new Pool(psqlEnv);
+// psqlPool.connect().then(() => {
+//   console.log("Connected to database");
+// });
+exports.default = pool;
