23562fc023cd88ad135c0acb15bf4f887acf6cbb
emdiso <emdisomma@gmail.com>
2022-12-06 22:01:43 -0500

    perty

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -7,7 +7,7 @@ import Collapse from '@mui/material/Collapse';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { styled } from '@mui/material';
+import { ImageListItem, ImageListItemBar, styled } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { baseServerUrl } from '../axios.service';
@@ -28,12 +28,18 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
     }),
 }));
 
-export function getIngredients(data: any){
+function getIngredients(data: any) {
     let ingredients: string[] = [];
     for (let i = 1; i < 16; i++) {
-        if (data.data[`strIngredient${i}`])
-            ingredients.push(data.data[`strIngredient${i}`])
+        if (data[`strIngredient${i}`]) {
+            let ingredient = data[`strIngredient${i}`];
+            ingredients.push(`${ingredient}, `)
+        }
     }
+    const ingredientsLength = ingredients.length;
+    // lol this line gets rid of the comma at the end
+    if (ingredientsLength > 0)
+        ingredients[ingredientsLength - 1] = ingredients[ingredientsLength - 1].substring(0, ingredients[ingredientsLength - 1].length - 2);
     return ingredients;
 }
 
@@ -48,47 +54,64 @@ function RecipeCard(data: any) {
 
     const handleModify = () => {
         if (data.data.idDrink) {
-            navigate('/recipe', { state: {id: 0, idDrink: data.data.idDrink}});
+            navigate('/recipe', { state: { id: 0, idDrink: data.data.idDrink } });
         }
         else {
-            navigate('/recipe', { state: {id: data.data.id, idDrink: 0}});
+            navigate('/recipe', { state: { id: data.data.id, idDrink: 0 } });
         }
     }
 
-    let ingredients: string[] = getIngredients(data);
-
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardHeader title={data.data.strDrink} />
-            {(data.data.strDrinkThumb || data.data.image_id) && <CardMedia
-                component="img"
-                height="194"
-                image={data.data.strDrinkThumb || `${baseServerUrl}/image/display?imageId=${data.data.image_id}`}
-                alt=""
-            />}
-            <CardContent>
-                Ingredients: {ingredients.join(", ")}
-            </CardContent>
-            <CardActions disableSpacing>
-                <button onClick={handleModify}>Modify</button>
-                <ExpandMore
-                    expand={expanded}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                >
-                    <ExpandMoreIcon />
-                </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <div>
+            <ImageListItem key={data.data.strDrink}>
+                {(data.data.strDrinkThumb || data.data.image_id) && <img
+                    src={data.data.strDrinkThumb || `${baseServerUrl}/image/display?imageId=${data.data.image_id}`}
+                    loading="lazy"
+                />}
+                <ImageListItemBar
+                    title={data.data.strDrink}
+                    subtitle={getIngredients(data.data)}
+                    actionIcon={
+                        <IconButton
+                            sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                            aria-label={`info about ${data.data.strDrink}`}
+                        >
+                        </IconButton>
+                    }
+                />
+            </ImageListItem>
+            {/* <Card sx={{ maxWidth: 345 }}>
+                <CardHeader title={data.data.strDrink} />
+                {(data.data.strDrinkThumb || data.data.image_id) && <CardMedia
+                    component="img"
+                    height="194"
+                    image={data.data.strDrinkThumb || `${baseServerUrl}/image/display?imageId=${data.data.image_id}`}
+                    alt=""
+                />}
                 <CardContent>
-                    <Typography paragraph>Recipe:</Typography>
-                    <Typography paragraph>
-                        {data.data.strInstructions}
-                    </Typography>
+                    Ingredients: {ingredients.join(", ")}
                 </CardContent>
-            </Collapse>
-        </Card>
+                <CardActions disableSpacing>
+                    <button onClick={handleModify}>Modify</button>
+                    <ExpandMore
+                        expand={expanded}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography paragraph>Recipe:</Typography>
+                        <Typography paragraph>
+                            {data.data.strInstructions}
+                        </Typography>
+                    </CardContent>
+                </Collapse>
+            </Card> */}
+        </div>
     );
 }
 
