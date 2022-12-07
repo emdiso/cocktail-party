7ad6855f08de3a3e79815e2e0ac8a264f0f506ef
cjc436 <97143146+cjc436@users.noreply.github.com>
2022-12-07 04:53:44 -0500

    abstracted promise handling code in service

--- src/axios.service.ts
+++ src/axios.service.ts
@@ -28,74 +28,56 @@ export function getGlobalSetStateMethod(name: string) {
 }
 
 export function get(endpoint: string, params: any, resHandler?: (res: AxiosResponse) => void, errHandler?: (err: AxiosError) => void) {
-    axios.get(
-        `${baseServerUrl}${endpoint}`,
-        {
-            params: params,
-            headers: {
-                "authorization": "Bearer " + authToken
+    handlePromise(
+        axios.get(
+            `${baseServerUrl}${endpoint}`,
+            {
+                params: params, 
+                headers: {
+                    "authorization": "Bearer " + authToken
+                }
             }
-        }
-    ).then(resHandler).catch((error: AxiosError) => {
-        let errMessage = "Error!";
-        if (error.response && error.response.data && typeof error.response.data === "string" && error.response.data !== "") errMessage = error.response.data;
-        const setMethodErrorAlert = getGlobalSetStateMethod("setErrorAlert");
-        setMethodErrorAlert !== undefined && setMethodErrorAlert(errMessage);
-        if (error.response && error.response.status === 401) {
-            localStorage.clear();
-            const setMethodLoggedIn = getGlobalSetStateMethod("setLoggedIn");
-            setMethodLoggedIn !== undefined && setMethodLoggedIn(false);
-            const setMethodUserInfo = getGlobalSetStateMethod("setUserInfo");
-            setMethodUserInfo !== undefined && setMethodUserInfo({ username: "", email: "" });
-        }
-        
-        (errHandler !== undefined ? errHandler : ((err: AxiosError) => {
-            // TO DO - write some error dialog / snackbar to display this information
-            console.log(err);
-        }))(error);
-    });
+        ),
+        resHandler,
+        errHandler
+    );
 }
 
 export function post(endpoint: string, data: any, params?: any, resHandler?: (res: AxiosResponse) => void, errHandler?: (err: AxiosError) => void) {
-    axios.post(
-        `${baseServerUrl}${endpoint}`,
-        data,
-        {
-            params: params, 
-            headers: {
-                "authorization": "Bearer " + authToken
+    handlePromise(
+        axios.post(
+            `${baseServerUrl}${endpoint}`,
+            data,
+            {
+                params: params, 
+                headers: {
+                    "authorization": "Bearer " + authToken
+                }
             }
-        }
-    ).then(resHandler).catch((error: AxiosError) => {
-        let errMessage = "Error!";
-        if (error.response && error.response.data && typeof error.response.data === "string" && error.response.data !== "") errMessage = error.response.data;
-        const setMethodErrorAlert = getGlobalSetStateMethod("setErrorAlert");
-        setMethodErrorAlert !== undefined && setMethodErrorAlert(errMessage);
-        if (error.response && error.response.status === 401) {
-            localStorage.clear();
-            const setMethodLoggedIn = getGlobalSetStateMethod("setLoggedIn");
-            setMethodLoggedIn !== undefined && setMethodLoggedIn(false);
-            const setMethodUserInfo = getGlobalSetStateMethod("setUserInfo");
-            setMethodUserInfo !== undefined && setMethodUserInfo({ username: "", email: "" });
-        }
-        
-        (errHandler !== undefined ? errHandler : ((err: AxiosError) => {
-            // TO DO - write some error dialog / snackbar to display this information
-            console.log(err);
-        }))(error);
-    });
+        ),
+        resHandler,
+        errHandler
+    );
 }
 
 export function del(endpoint: string, params?: any, resHandler?: (res: AxiosResponse) => void, errHandler?: (err: AxiosError) => void) {
-    axios.delete(
-        `${baseServerUrl}${endpoint}`,
-        {
-            params: params || {},
-            headers: {
-                "authorization": "Bearer " + authToken
+    handlePromise(
+        axios.delete(
+            `${baseServerUrl}${endpoint}`,
+            {
+                params: params || {},
+                headers: {
+                    "authorization": "Bearer " + authToken
+                }
             }
-        }
-    ).then(resHandler).catch((error: AxiosError) => {
+        ),
+        resHandler,
+        errHandler
+    );
+}
+
+const handlePromise = (promise: Promise<AxiosResponse<any, any>>, resHandler?: (res: AxiosResponse) => void, errHandler?: (err: AxiosError) => void) => {
+    promise.then(resHandler).catch((error: AxiosError) => {
         let errMessage = "Error!";
         if (error.response && error.response.data && typeof error.response.data === "string" && error.response.data !== "") errMessage = error.response.data;
         const setMethodErrorAlert = getGlobalSetStateMethod("setErrorAlert");
@@ -108,10 +90,6 @@ export function del(endpoint: string, params?: any, resHandler?: (res: AxiosResp
             setMethodUserInfo !== undefined && setMethodUserInfo({ username: "", email: "" });
         }
 
-        
-        (errHandler !== undefined ? errHandler : ((err: AxiosError) => {
-            // TO DO - write some error dialog / snackbar to display this information
-            console.log(err);
-        }))(error);
+        errHandler !== undefined && errHandler(error);
     });
-}
+};
